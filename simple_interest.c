ab873e2bdda45ad1ed396c5d46ec7af8842d4d9c
jenishtada <jenishtada3425@gmail.com>
2023-05-02 16:04:00 +0530

    Module 2 & 3 complet

---
+++ simple_interest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int p = 50000;
+    float r = 2;
+    int n = 1;
+
+    int i = p*r*n/100;
+
+    printf("your interest is : %d\n",i);
+}
---
+++ square_cube.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10;
+    
+    printf("the square is : %d\n",a*a);
+    printf("the cube is : %d\n",a*a*a);
+
+}
